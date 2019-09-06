65a948eb75df8f11b66e61b0664f5a79c7b5329b
wangshaoyong <418761605@qq.com>
2019-09-05 22:16:59 -0400

    update

--- 447. Number of Boomerangs.py
+++ 447. Number of Boomerangs.py
@@ -1,34 +1,23 @@
 import copy
 
-points = [[0, 0], [1, 0], [2, 0]]
+points = [[0,0],[1,0],[-1,0],[0,1],[0,-1]]
 
-d = {}
-h = set()
+d = {i: {} for i in range(len(points))}
+
+for i in range(len(points) - 1):
+    for j in range(i + 1, len(points)):
+        dis = (points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2
+        d[i][dis] = d[i].get(dis, 0) + 1
+        d[j][dis] = d[j].get(dis, 0) + 1
 
 ans = 0
+
 for i in range(len(points)):
-    for j in range(len(points) - 1):
-        if j == i:
-            continue
-        for k in range(j + 1, len(points)):
-            if k == i:
-                continue
-            if (i, j) not in h:
-                i_j = (points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2
-                d[(i, j)] = i_j
-                d[(j, i)] = i_j
-                h.add((i, j))
-                h.add((j, i))
-            else:
-                i_j = d.get((i, j))
-            if (i, k) not in h:
-                i_k = (points[i][0] - points[k][0]) ** 2 + (points[i][1] - points[k][1]) ** 2
-                d[(i, k)] = i_k
-                d[(k, i)] = i_k
-                h.add((i, k))
-                h.add((k, i))
-            else:
-                i_k = d.get((i, k))
-            if i_j == i_k:
-                ans += 1
-print(ans * 2)
+    for ele in d.get(i).items():
+        if ele[1] == 2:
+            ans += 2
+        elif ele[1] > 2:
+            ans += (ele[1]) * (ele[1] - 1)
+
+print(d)
+print(ans)
